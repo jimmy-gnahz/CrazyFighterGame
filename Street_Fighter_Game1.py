c5b6c2c5e74392fed3dafa80bc3a5c85c620fd49
Jimmy Zhang <jimmyjimbozhang@gmail.com>
2015-09-04 14:37:03 -0700

    added author and GREEN

--- Street_Fighter_Game1.py
+++ Street_Fighter_Game1.py
@@ -1,3 +1,5 @@
+# Author : Philip Xia, Jimmy Zhang
+
 #//////////////////////////////////////////////////////////////////////////////#
 #                                                                              #
 #                                                                              #
@@ -29,6 +31,7 @@ WHITE = [255, 255, 255]
 RED = [255, 0 , 0]
 BLUE = [0, 0, 255]
 YELLOW = [255, 255, 0]
+GREEN = [ 0 , 255 , 0 ]
 "///other values///"
 size = [1000,644]
 screen = pygame.display.set_mode(size)
